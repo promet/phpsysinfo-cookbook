1bdef2ed056d8c69c079d7e8aa23d5df19cdce2b
Greg Palmier <gregpalmier@gmail.com>
2013-10-04 12:29:24 -0500

    init new.  clean out password annoyances.  this is for devs to add with berks, not production machines.

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,12 +15,12 @@ include_recipe 'apache2::mod_php5'
 bash "create_sitedir" do
  user "root"
  code <<-EOH
- mkdir -p #{node[:apache][:docroot_dir]}/promet
+ mkdir -p #{node[:apache][:docroot_dir]}/phpinfo
  EOH
 end
 
 # A basic phpinfo.php file
-cookbook_file "#{node[:apache][:docroot_dir]}/promet/phpinfo.php" do
+cookbook_file "#{node[:apache][:docroot_dir]}/phpinfo/phpinfo.php" do
   source "phpinfo.php"
   mode 0644
   owner "root"
@@ -29,7 +29,7 @@ end
 
 # Clone the phpsysinfo repo from GitHub and put it under the
 # default apache vhost location (under an assumption here, yes)
-git "#{node[:apache][:docroot_dir]}/promet/phpsysinfo" do
+git "#{node[:apache][:docroot_dir]}/phpinfo/phpsysinfo" do
    repository "https://github.com/rk4an/phpsysinfo.git"
    user "root"
    action :checkout
@@ -37,7 +37,7 @@ end
 
 # Create the phpsysinfo.ini file (defaults, or it would
 # be a templated thing with attributes)
-cookbook_file "#{node[:apache][:docroot_dir]}/promet/phpsysinfo/phpsysinfo.ini" do
+cookbook_file "#{node[:apache][:docroot_dir]}/phpinfo/phpsysinfo/phpsysinfo.ini" do
   source "phpsysinfo.ini"
   mode 0644
   owner "root"
@@ -53,43 +53,35 @@ cookbook_file "#{node[:apache][:docroot_dir]}/index.html" do
   action :create
 end
 
-# add basic demo / demo credentials
-cookbook_file "#{node[:apache][:docroot_dir]}/promet/.htpasswd" do
-  source "htpasswd"
+cookbook_file "#{node[:apache][:docroot_dir]}/phpinfo/phpinfo_memcache.php" do
+  source "phpinfo_memcache.php"
   mode 0644
   owner "root"
   action :create
 end
 
-cookbook_file "#{node[:apache][:docroot_dir]}/promet/promet_memcache.php" do
-  source "promet_memcache.php"
+cookbook_file "#{node[:apache][:docroot_dir]}/phpinfo/phpinfo_apc.php" do
+  source "phpinfo_apc.php"
   mode 0644
   owner "root"
   action :create
 end
 
-cookbook_file "#{node[:apache][:docroot_dir]}/promet/promet_apc.php" do
-  source "promet_apc.php"
-  mode 0644
-  owner "root"
-  action :create
-end
-
-case node[:platform]
-  when "ubuntu","debian"
-    template "#{node[:apache][:dir]}/conf.d/phpsysinfo.conf" do
-    source "phpsysinfo.conf.erb"
-    owner "root"
-    group 0
-    mode 00644
-    notifies :restart, "service[apache2]"
-  end
-  when "centos","redhat"
-    template "#{node[:apache][:dir]}/conf.d/phpsysinfo.conf" do
-    source "phpsysinfo.conf.erb"
-    owner "root"
-    group 0
-    mode 00644
-    notifies :restart, "service[httpd]"
-  end
-end
+#case node[:platform]
+#  when "ubuntu","debian"
+#    template "#{node[:apache][:dir]}/conf.d/phpsysinfo.conf" do
+#    source "phpsysinfo.conf.erb"
+#    owner "root"
+#    group 0
+#    mode 00644
+#    notifies :restart, "service[apache2]"
+#  end
+#  when "centos","redhat"
+#    template "#{node[:apache][:dir]}/conf.d/phpsysinfo.conf" do
+#    source "phpsysinfo.conf.erb"
+#    owner "root"
+#    group 0
+#    mode 00644
+#    notifies :restart, "service[httpd]"
+#  end
+#end
